116dc9a81292b14664217a5e77f68cb3d2b8f2da
pimlu <pimlu@users.noreply.github.com>
2015-08-09 20:57:25 -0400

    add itoa and test code

--- src/main.asm
+++ src/main.asm
@@ -2,16 +2,22 @@
 
 	.text
 _start:
-	#say hello
-	lea	rdi, [message]
-	mov	rsi, offset msg_size
+	mov	rdi, offset magic
+	mov	rsi, offset magic_size
 	call	print
+	
+	mov	rdi, 1234
+	call	itoa
+	mov	rdi, rax
+	mov	rsi, rdx
+	call	println
+	
 
 	#exit 0
 	mov	rdi, 0
 	call	exit
 	
 	.data
-message:
-	.ascii	"Hello, world\n"
-	.set msg_size, .-message
+magic:
+	.ascii	"The magic number is: "
+	.set magic_size, .-magic
--- src/utils.asm
+++ src/utils.asm
@@ -1,4 +1,4 @@
-	.global print, exit
+	.global print, println, exit, itoa
 	
 	.text
 #sys_writes to stdout.
@@ -10,7 +10,65 @@ print:
 	mov	rdi, 1
 	syscall
 	ret
+
+println:
+	cmp	rsi, 0
+	jz	.skip_print
+	call print
+.skip_print:
+	mov	rdi, offset newline
+	mov	rsi, 1
+	call print
+	ret
+	
 #sys_exits with code. rdi: code
 exit:
 	mov	rax, 60
 	syscall
+
+
+
+#params: rdi: i
+#returns: rax: char* (reused); rdx: length
+itoa:
+	push	rbx
+	mov	rax, rdi
+	mov	rbx, 10
+	mov	rcx, 1
+#count the digits in our number
+.count_loop:
+	cmp	rax, 10
+	jb	.count_break
+	
+	inc	rcx
+	
+	mov	rdx, 0
+	div	rbx
+	jmp	.count_loop
+.count_break:
+	#rcx is now the length, store it in r8 as a backup
+	mov	r8, rcx
+	mov	rax, rdi
+#write the digits starting at the end
+.digit_loop:
+	mov	rdx, 0
+	div	rbx
+	
+	add	rdx, '0'
+	dec	rcx
+	mov	[numtext + rcx], dl
+	jnz	.digit_loop
+.digit_break:
+	#put the address of the text in rax
+	mov	rax, offset numtext
+	#return the length and restore rbx
+	mov	rdx, r8
+	pop	rbx
+	ret
+
+
+	.data
+newline:
+	.ascii 	"\n"
+numtext:
+	.space 32
